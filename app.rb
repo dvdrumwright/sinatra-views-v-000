e2c33e6703cc778d05802b7874c59d91a70ec821
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-11 01:17:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,16 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
+ 	 erb :index
+  end
+
+ get '/' do
      "<h1>Hello World</h1>"
 		end
-
-		get '/' do
-      erb :index
-    end
 
     get "/info" do
       erb :info
     end
 
-		
+
 end
